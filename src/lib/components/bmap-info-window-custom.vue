1a004ef65c98e047fb1735f2bb14efa660d4738a
guyangyang <407042815@qq.com>
2021-07-23 15:13:30 +0800

    自定义弹框增加锚点控制

--- src/lib/components/bmap-info-window-custom.vue
+++ src/lib/components/bmap-info-window-custom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="infoWindow" class="bmap-info-window-custom" :style="computedStyle" :class="{custom: isCustom}">
+  <div ref="infoWindow" class="bmap-info-window-custom" :style="computedStyle" :class="[isCustom ? 'custom' : '', anchorClass]">
     <div class="arrow" v-if="!isCustom"></div>
     <slot />
   </div>
@@ -41,6 +41,14 @@ export default {
       type: Boolean,
       default: true
     },
+    anchor: {
+      type: String,
+      default: 'bottom',
+      validator: (value) => {
+        // auto(自动根据可显示位置进行处理，优先bottom)、top(顶部中心)、right(右侧中心)、bottom(底部中心)、left(左侧中心) 、top-left(左上角)、top-right(右上角)、bottom-left(左下角)、bottom-right(右下角)
+        return ['auto', 'top', 'right', 'bottom', 'left', 'top-left', 'top-right', 'bottom-left', 'bottom-right'].indexOf(value) > -1;
+      }
+    },
     events: {
       type: Object,
       default() {
@@ -57,8 +65,18 @@ export default {
         left: 0,
         top: 0
       },
+      anchorClass: 'bottom',
       savePosition: null,
       saveVisible: false,
+      saveEnableAutoPan: true,
+      savePixel: {
+        x: 0,
+        y: 0
+      },
+      mapSize: {
+        width: 0,
+        height: 0
+      },
       handlers: {
         position(value) {
           _this.savePosition = value;
@@ -103,30 +121,34 @@ export default {
         addEventListener: ()=>{}
       };
       this.saveVisible = this.visible;
+      this.saveEnableAutoPan = this.enableAutoPan;
+      this.mapSize = this.$bmap.getContainerSize();
       if (!options.position) {
         return;
       }
       this.emitEvent();
       this.savePosition = options.position;
       this.calcPosition();
-      this.$nextTick(() => {
-        let height = this.$refs.infoWindow.offsetHeight + 11;
-        let width = this.$refs.infoWindow.offsetWidth;
-        let pixel = this.$bmap.pointToOverlayPixel(this.savePosition);
-        let leftOffset = pixel.x - width / 2 + this.offset[0];
-        let topOffset = pixel.y - height + this.offset[1];
-        let panX = 0;
-        let panY = 0;
-        if (leftOffset < 0) {
-          panX = Math.abs(leftOffset) + 10;
-        }
-        if (topOffset < 0) {
-          panY = Math.abs(topOffset) + 10;
-        }
-        if (panX !== 0 || panY !== 0) {
-          this.$bmap.panBy(panX, panY);
-        }
-      });
+      if (this.enableAutoPan) {
+        this.$nextTick(() => {
+          let height = this.$refs.infoWindow.offsetHeight + 11;
+          let width = this.$refs.infoWindow.offsetWidth;
+          let pixel = this.$bmap.pointToOverlayPixel(this.savePosition);
+          let leftOffset = pixel.x - width / 2 + this.offset[0];
+          let topOffset = pixel.y - height + this.offset[1];
+          let panX = 0;
+          let panY = 0;
+          if (leftOffset < 0) {
+            panX = Math.abs(leftOffset) + 10;
+          }
+          if (topOffset < 0) {
+            panY = Math.abs(topOffset) + 10;
+          }
+          if (panX !== 0 || panY !== 0) {
+            this.$bmap.panBy(panX, panY);
+          }
+        });
+      }
       this.bindEvent();
     },
     bindEvent() {
@@ -153,6 +175,7 @@ export default {
       this.calcPosition();
     },
     resizeMap() {
+      this.mapSize = this.$bmap.getContainerSize();
       this.calcPosition();
     },
     clickMap() {
@@ -163,9 +186,45 @@ export default {
       }
     },
     calcPosition() {
+      if (!this.savePosition) {
+        return;
+      }
       let pixel = this.$bmap.pointToOverlayPixel(this.savePosition);
+      this.savePixel = pixel;
       this.styleObj.left = (pixel.x + this.offset[0]) + 'px';
       this.styleObj.top = (pixel.y + this.offset[1]) + 'px';
+      this.$nextTick(() => {
+        this.calcAnchor();
+      });
+    },
+    calcAnchor() {
+      if (this.anchor !== 'auto' || this.enableAutoPan) {
+        this.anchorClass = this.anchor !== 'auto' ? this.anchor : 'bottom';
+        return;
+      }
+      let mapSize = this.mapSize;
+      let mapWidth = mapSize.width;
+      let mapHeight = mapSize.height;
+      let arrowOffset = 11;
+      let height = this.$refs.infoWindow.offsetHeight;
+      let width = this.$refs.infoWindow.offsetWidth;
+      let pointX = this.savePixel.x;
+      let pointY = this.savePixel.y;
+      let anchor = '';
+      if ((height + arrowOffset) <= pointY && (width / 2) <= pointX) {
+        anchor = 'bottom';
+      } else if ((width + arrowOffset) <= pointX && (height / 2) <= pointY) {
+        anchor = 'right';
+      } else if ((height + arrowOffset + pointY) <= mapHeight && (width / 2) <= pointX) {
+        anchor = 'top';
+      } else if ((height / 2) <= pointY && (mapWidth - pointX) >= (width + arrowOffset)) {
+        anchor = 'left';
+      }
+      if (!anchor) {
+        anchor = 'bottom';
+      }
+      this.anchorClass = anchor;
+
     },
     emitEvent() {
       if (this.events && this.events.close && !this.saveVisible) {
@@ -184,19 +243,81 @@ export default {
     box-shadow: 0 5px 10px 0 rgba(65, 84, 102, 0.09);
     border-radius: 5px;
     z-index: 200;
-    transform: translate(-50%, calc(-100% - 11px));
 
-    .arrow{
-      position: absolute;
-      box-shadow: 0 5px 7px -1px rgb(238 238 238 / 50%);
-      width: 0;
-      height: 0;
-      border-left: 6px solid transparent;
-      border-right: 6px solid transparent;
-      border-top: 11px solid #F5F8FF;
-      bottom: -11px;
-      left: 50%;
-      transform: translateX(-50%);
+    &.top{
+      transform: translate(-50%, 11px);
+
+      .arrow{
+        position: absolute;
+        box-shadow: 0 5px 7px -1px rgb(238 238 238 / 50%);
+        width: 0;
+        height: 0;
+        border-left: 6px solid transparent;
+        border-right: 6px solid transparent;
+        border-bottom: 11px solid #F5F8FF;
+        top: -11px;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+    &.right{
+      transform: translate(calc(-100% - 11px), -50%);
+
+      .arrow{
+        position: absolute;
+        box-shadow: 0 5px 7px -1px rgb(238 238 238 / 50%);
+        width: 0;
+        height: 0;
+        border-top: 6px solid transparent;
+        border-bottom: 6px solid transparent;
+        border-left: 11px solid #F5F8FF;
+        right: -11px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+    &.bottom{
+      transform: translate(-50%, calc(-100% - 11px));
+
+      .arrow{
+        position: absolute;
+        box-shadow: 0 5px 7px -1px rgb(238 238 238 / 50%);
+        width: 0;
+        height: 0;
+        border-left: 6px solid transparent;
+        border-right: 6px solid transparent;
+        border-top: 11px solid #F5F8FF;
+        bottom: -11px;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+    &.left{
+      transform: translate(11px, -50%);
+
+      .arrow{
+        position: absolute;
+        box-shadow: 0 5px 7px -1px rgb(238 238 238 / 50%);
+        width: 0;
+        height: 0;
+        border-top: 6px solid transparent;
+        border-bottom: 6px solid transparent;
+        border-right: 11px solid #F5F8FF;
+        left: -11px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+    &.top-left{
+    }
+    &.top-right{
+      transform: translate(-100%, 0);
+    }
+    &.bottom-left{
+      transform: translate(0, -100%);
+    }
+    &.bottom-right{
+      transform: translate(-100%, -100%);
     }
 
     &.custom{
