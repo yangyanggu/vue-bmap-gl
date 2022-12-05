cc08667aafcb0b1a4db17862943e6ba0605e3b35
gu <407042815@qq.com>
2022-12-05 17:55:57 +0800

    修改文档

--- src/lib/services/lazy-bmap-api-loader.js
+++ src/lib/services/lazy-bmap-api-loader.js
@@ -24,11 +24,11 @@ export default class AMapAPILoader {
 
   load() {
     if (this._scriptLoadingPromise) return this._scriptLoadingPromise;
-    if(this._config.offline){
+    if (this._config.offline) {
       this._scriptLoadingPromise = new Promise(resolve => {
-        resolve()
-      })
-      return this._scriptLoadingPromise
+        resolve();
+      });
+      return this._scriptLoadingPromise;
     }
     const script = this._document.createElement('script');
     script.type = 'text/javascript';
